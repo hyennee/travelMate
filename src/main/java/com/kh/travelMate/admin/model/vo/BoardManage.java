725243c7c2d517112d0771bb26aa06d37c642918
udonmania2018 <winternebel@gmail.com>
2018-11-13 21:24:39 +0900

    1. 게시물 관리 전체 목록 구현

--- src/main/java/com/kh/travelMate/admin/model/vo/BoardManage.java
+++ src/main/java/com/kh/travelMate/admin/model/vo/BoardManage.java
@@ -14,11 +14,19 @@ public class BoardManage implements Serializable{
 	private String status;
 	private int board_level;
 	private int ref_no;
+	private String email;
+	private String user_Name;
+	private String nick_Name;
+	private String phone;
+	private String birthDay;
+	private String gender;
+	private Date enroll_Date;
 	
 	public BoardManage() {}
 
 	public BoardManage(int board_no, int writer, String category, String title, String content, Date board_date,
-			String status, int board_level, int ref_no) {
+			String status, int board_level, int ref_no, String email, String user_Name, String nick_Name, String phone,
+			String birthDay, String gender, Date enroll_Date) {
 		super();
 		this.board_no = board_no;
 		this.writer = writer;
@@ -29,6 +37,13 @@ public class BoardManage implements Serializable{
 		this.status = status;
 		this.board_level = board_level;
 		this.ref_no = ref_no;
+		this.email = email;
+		this.user_Name = user_Name;
+		this.nick_Name = nick_Name;
+		this.phone = phone;
+		this.birthDay = birthDay;
+		this.gender = gender;
+		this.enroll_Date = enroll_Date;
 	}
 
 	public int getBoard_no() {
@@ -103,12 +118,76 @@ public class BoardManage implements Serializable{
 		this.ref_no = ref_no;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUser_Name() {
+		return user_Name;
+	}
+
+	public void setUser_Name(String user_Name) {
+		this.user_Name = user_Name;
+	}
+
+	public String getNick_Name() {
+		return nick_Name;
+	}
+
+	public void setNick_Name(String nick_Name) {
+		this.nick_Name = nick_Name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getBirthDay() {
+		return birthDay;
+	}
+
+	public void setBirthDay(String birthDay) {
+		this.birthDay = birthDay;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getEnroll_Date() {
+		return enroll_Date;
+	}
+
+	public void setEnroll_Date(Date enroll_Date) {
+		this.enroll_Date = enroll_Date;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 	@Override
 	public String toString() {
 		return "BoardManage [board_no=" + board_no + ", writer=" + writer + ", category=" + category + ", title="
 				+ title + ", content=" + content + ", board_date=" + board_date + ", status=" + status
-				+ ", board_level=" + board_level + ", ref_no=" + ref_no + "]";
-	}	
+				+ ", board_level=" + board_level + ", ref_no=" + ref_no + ", email=" + email + ", user_Name="
+				+ user_Name + ", nick_Name=" + nick_Name + ", phone=" + phone + ", birthDay=" + birthDay + ", gender="
+				+ gender + ", enroll_Date=" + enroll_Date + "]";
+	}
+
+
 	
 
 }
